b00b19a0155c032a82c43a24fe0fd46dd2ba2779
kevinsims1 <kevindsims1@gmail.com>
2020-06-24 14:45:53 -0700

    items lookin good

--- src/components/home.js
+++ src/components/home.js
@@ -1,39 +1,46 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../css/bare.css'
+import Shirt from '../assets/shirt.jpeg'
 
 const Home = () => {
   return (
     <div className="homeWrapper">
         <div className="items">
         <div className='item'>
+            
+            <img src={Shirt}/>
             <div>Title</div>
-            <div>Picture</div>
             <div>Price</div>
           </div>
           <div className='item'>
+            
+            <img src={Shirt}/>
             <div>Title</div>
-            <div>Picture</div>
             <div>Price</div>
           </div>
           <div className='item'>
+            
+            <img src={Shirt}/>
             <div>Title</div>
-            <div>Picture</div>
             <div>Price</div>
           </div>
           <div className='item'>
+            
+            <img src={Shirt}/>
             <div>Title</div>
-            <div>Picture</div>
             <div>Price</div>
           </div>
           <div className='item'>
+            
+            <img src={Shirt}/>
             <div>Title</div>
-            <div>Picture</div>
             <div>Price</div>
           </div>
           <div className='item'>
+            
+            <img src={Shirt}/>
             <div>Title</div>
-            <div>Picture</div>
             <div>Price</div>
           </div>
           
