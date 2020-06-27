d9ef8d327a992edbf24ad79a7559a1b5145eacd3
kevinsims1 <kevindsims1@gmail.com>
2020-06-27 08:43:25 -0700

    k

--- src/components/home.js
+++ src/components/home.js
@@ -6,45 +6,44 @@ import Shirt from '../assets/shirt.jpeg'
 const Home = () => {
   return (
     <div className="homeWrapper">
-        <div className="items">
-        <div className='item'>
-            
-            <img src={Shirt}/>
-            <div>Title</div>
-            <div>Price</div>
-          </div>
-          <div className='item'>
-            
-            <img src={Shirt}/>
-            <div>Title</div>
-            <div>Price</div>
-          </div>
-          <div className='item'>
-            
-            <img src={Shirt}/>
-            <div>Title</div>
-            <div>Price</div>
-          </div>
-          <div className='item'>
-            
-            <img src={Shirt}/>
-            <div>Title</div>
-            <div>Price</div>
-          </div>
-          <div className='item'>
-            
-            <img src={Shirt}/>
-            <div>Title</div>
-            <div>Price</div>
-          </div>
-          <div className='item'>
-            
-            <img src={Shirt}/>
-            <div>Title</div>
-            <div>Price</div>
-          </div>
-          
+      <div className="items">
+        <div className="item">
+          <img src={Shirt} />
+          <div>Title</div>
+          <div>Description</div>
+          <div>Price</div>
         </div>
+        <div className="item">
+          <img src={Shirt} />
+          <div>Title</div>
+          <div>Description</div>
+          <div>Price</div>
+        </div>
+        <div className="item">
+          <img src={Shirt} />
+          <div>Title</div>
+          <div>Description</div>
+          <div>Price</div>
+        </div>
+        <div className="item">
+          <img src={Shirt} />
+          <div>Title</div>
+          <div>Description</div>
+          <div>Price</div>
+        </div>
+        <div className="item">
+          <img src={Shirt} />
+          <div>Title</div>
+          <div>Description</div>
+          <div>Price</div>
+        </div>
+        <div className="item">
+          <img src={Shirt} />
+          <div>Title</div>
+          <div>Description</div>
+          <div>Price</div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import '../css/bare.css'
 const Nav = () => {
   return (
     <div className="nav">
       <h2>Grails For Sell</h2>
       <div className="options">
-        <Link to="/link">Vintage</Link>
-        <Link to="/link">Streetwear</Link>
-        <Link to="/link">Hype</Link>
+        <Link to="/link" className='navItem'>Vintage</Link>
+        <Link to="/link" className='navItem'>Streetwear</Link>
+        <Link to="/link" className='navItem'>Hype</Link>
       </div>
     </div>
   )
