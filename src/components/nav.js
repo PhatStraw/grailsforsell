a550f01dcdd8e14914d5a081faa67e3cbda89a34
kevinsims1 <kevindsims1@gmail.com>
2020-08-11 13:35:55 -0700

    cap convention

--- src/components/nav.js
+++ src/components/nav.js
@@ -43,27 +43,3 @@ const Nav = () => {
 }
 
 export default Nav
-
-// import React, {useEffect} from 'react'
-// import { NavLink } from 'react-router-dom'
-// import {FaShoppingCart} from 'react-icons/fa'
-// import '../css/bare.css'
-// const Nav = () => {
-  
-//   return (
-//     <div className="nav">
-//       <h2 className="header">
-//         <div style={{ display: 'hidden'}}></div>
-//         <NavLink to="/" className="navTitle">Grails For Sell</NavLink>
-//         <NavLink to={'/checkout'} className="navTitle"><FaShoppingCart size={20}/></NavLink>
-//       </h2>
-//       <div className="options">
-//         <NavLink to="/vintage" className='navItem'>Vintage</NavLink>
-//         <NavLink to="/streetwear" className='navItem'>Streetwear</NavLink>
-//         <NavLink to="/hype" className='navItem'>Hype</NavLink>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Nav
