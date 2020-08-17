77623960f98f6f878f32d7e3777a1bf544fb00d8
kevinsims1 <kevindsims1@gmail.com>
2020-08-16 21:17:16 -0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,54 @@
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
-const dotenv = require('dotenv')
-// call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed
-
-// reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next])
-  return prev
-}, {})
-
+const path = require( 'path' );
 module.exports = {
-  context: __dirname,
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '/',
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve( __dirname, 'dist' ),
+      filename: 'main.js',
+      publicPath: '/',
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-    }),
-    new webpack.DefinePlugin(envKeys)
-  ],
-}
+        {
+         test:/\.html$/,
+         use: [
+             {
+                 loader: "html-loader"
+             }
+         ]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+      ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: path.resolve( __dirname, 'public/index.html' ),
+          filename: 'index.html'
+        }),
+        new Dotenv()
+    ]
+  };
